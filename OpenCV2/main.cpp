681a6db34b3abbefcd51829fa04831900d5e791f
Diego Tomas Perez <diegotoamsperez@usal.es>
2018-05-26 18:29:00 +0200

    Arreglo git

--- OpenCV2/main.cpp
+++ OpenCV2/main.cpp
@@ -98,9 +98,9 @@ void createTrackbars() {
 Point getPointingAngle(Object centralPoint, vector<Point> contours) {
 	if (contours.size() != 3) return NULL;
 	Point extremePoint;
-	int distanceP0 = sqrt(pow( contours[0].x - centralPoint.getXPos(),2) + pow( contours[0].y - centralPoint.getYPos(),2));
-	int distanceP1 = sqrt(pow( contours[1].x - centralPoint.getXPos(),2) + pow( contours[1].y - centralPoint.getYPos(),2));
-	int distanceP2 = sqrt(pow( contours[2].x - centralPoint.getXPos(),2) + pow( contours[2].y - centralPoint.getYPos(),2));
+	int distanceP0 = sqrt(pow(contours[0].x - centralPoint.getXPos(), 2) + pow(contours[0].y - centralPoint.getYPos(), 2));
+	int distanceP1 = sqrt(pow(contours[1].x - centralPoint.getXPos(), 2) + pow(contours[1].y - centralPoint.getYPos(), 2));
+	int distanceP2 = sqrt(pow(contours[2].x - centralPoint.getXPos(), 2) + pow(contours[2].y - centralPoint.getYPos(), 2));
 
 
 	//Calc maximum distance
@@ -110,21 +110,22 @@ Point getPointingAngle(Object centralPoint, vector<Point> contours) {
 		extremePoint = contours[0];
 		distTemp = distanceP0;
 		temp = 0;
-	}else {
+	}
+	else {
 		extremePoint = contours[1];
 		distTemp = distanceP0;
 		temp = 1;
 	}
 	if (distTemp > distanceP2) {
 		extremePoint = contours[temp];
-	
+
 	}
 
 	else {
 		extremePoint = contours[2];
 	}
 
-	
+
 	return extremePoint;
 }
 
@@ -136,10 +137,10 @@ void drawObject(vector<Object> theObjects, Mat &frame, Mat &temp, vector< vector
 		cv::putText(frame, intToString(theObjects.at(i).getXPos()) + " , " + intToString(theObjects.at(i).getYPos()), cv::Point(theObjects.at(i).getXPos(), theObjects.at(i).getYPos() + 20), 1, 1, theObjects.at(i).getColor());
 		cv::putText(frame, theObjects.at(i).getType(), cv::Point(theObjects.at(i).getXPos(), theObjects.at(i).getYPos() - 20), 1, 2, theObjects.at(i).getColor());
 		if (contours[i].size() == 3) {
-			Point dir=getPointingAngle(theObjects.at(i), contours[i]);
+			Point dir = getPointingAngle(theObjects.at(i), contours[i]);
 			cv::circle(frame, dir, 5, theObjects.at(i).getColor());
 		}
-		
+
 	}
 }
 
@@ -167,6 +168,9 @@ void morphOps(Mat &thresh) {
 	dilate(thresh, thresh, dilateElement);
 	dilate(thresh, thresh, dilateElement);
 }
+
+
+
 void trackFilteredObject(Mat threshold, Mat HSV, Mat &cameraFeed)
 {
 	vector <Object> objects;
@@ -220,6 +224,87 @@ void trackFilteredObject(Mat threshold, Mat HSV, Mat &cameraFeed)
 	}
 }
 
+vector<Point> trackPlayer(Mat &cameraFeed) {
+	Mat threshold;
+	Mat HSV;
+	Object theObject("blue");
+
+	//first find blue objects
+	cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
+	cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
+	inRange(HSV, theObject.getHSVmin(), theObject.getHSVmax(), threshold);
+	morphOps(threshold);
+
+	vector <Object> objects;
+	Mat temp;
+	threshold.copyTo(temp);
+	//these two vectors needed for output of findContours
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	//find contours of filtered image using openCV findContours function
+	findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if (numObjects<MAX_NUM_OBJECTS) {
+			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+
+				//if the area is less than 20 px by 20px then it is probably just noise
+				//if the area is the same as the 3/2 of the image size, probably just a bad filter
+				//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if (area>MIN_OBJECT_AREA) {
+
+					Object object;
+
+					object.setXPos(moment.m10 / area);
+					object.setYPos(moment.m01 / area);
+					object.setType(theObject.getType());
+					object.setColor(theObject.getColor());
+
+					objects.push_back(object);
+
+					objectFound = true;
+
+				}
+				else objectFound = false;
+			}
+			//let user know you found an object
+			if (objectFound == true) {
+				//draw object location on screen
+
+				//Approx each cotour object to a geometric figure
+				for (int i = 0; i<contours.size(); i++)
+					cv::approxPolyDP(contours[i], contours[i], 0.1*cv::arcLength(contours[i], true), true);
+
+				Object playerP0 = objects[0];
+				Point dir = getPointingAngle(playerP0, contours[0]);
+
+				vector<Point> player;
+				player.push_back(Point(playerP0.getXPos(), playerP0.getYPos()));
+				player.push_back(dir);
+				return player;
+
+
+			}
+
+		}
+		else putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+	}
+
+	//No return value
+	vector<Point> voidPlayer;
+	voidPlayer.push_back(Point(0, 0));
+	voidPlayer.push_back(Point(0, 0));
+	return voidPlayer;
+}
 
 void trackFilteredObject(Object theObject, Mat threshold, Mat HSV, Mat &cameraFeed) {
 
@@ -234,7 +319,7 @@ void trackFilteredObject(Object theObject, Mat threshold, Mat HSV, Mat &cameraFe
 
 	//vector< vector<Point> > newContours;
 
-	
+
 
 
 	//use moments method to find our filtered object
@@ -274,9 +359,9 @@ void trackFilteredObject(Object theObject, Mat threshold, Mat HSV, Mat &cameraFe
 				//draw object location on screen
 
 				//Approx each cotour object to a geometric figure
-				for(int i=0;i<contours.size();i++)
+				for (int i = 0; i<contours.size(); i++)
 					cv::approxPolyDP(contours[i], contours[i], 0.1*cv::arcLength(contours[i], true), true);
-				
+
 
 				drawObject(objects, cameraFeed, temp, contours, hierarchy);
 			}
@@ -288,158 +373,76 @@ void trackFilteredObject(Object theObject, Mat threshold, Mat HSV, Mat &cameraFe
 
 void calibrateLEDs(double angle, double distance)
 {
-  float intense = 1-(distance / distMax);    
+	float intense = 1 - (distance / distMax);
 
-  if ((angle > 0 && angle <= 11'25) || (angle > 348'75 && angle <= 360)){
-    ledBelt[0] = intense;
-  }
-  else if (angle > 11'25 && angle <= 33'75){
-    ledBelt[1] = intense;
-  }
-  else if (angle > 33'75 && angle <= 56'25){
-    ledBelt[2] = intense;
-  }
-  else if (angle > 56'25 && angle <= 78'75){
-    ledBelt[3] = intense;
-  }
-  else if (angle > 78'75 && angle <= 101'25){
-    ledBelt[4] = intense;
-  }
-  else if (angle > 101'25 && angle <= 123'75){
-    ledBelt[5] = intense;
-  }
-  else if (angle > 123'75 && angle <= 146'25){
-    ledBelt[6] = intense;
-  }
-  else if (angle > 146'25 && angle <= 168'75){
-    ledBelt[7] = intense;
-  }
-  else if (angle > 168'75 && angle <= 191'25){
-    ledBelt[8] = intense;
-  }
-  else if (angle > 191'25 && angle <= 213'75){
-    ledBelt[9] = intense;
-  }
-  else if (angle > 213'75 && angle <= 236'25){
-    ledBelt[10] = intense;
-  }
-  else if (angle > 236'25 && angle <= 258'75){
-    ledBelt[11] = intense;
-  }
-  else if (angle > 258'75 && angle <= 281'25){
-    ledBelt[12] = intense;
-  }
-  else if (angle > 281'25 && angle <= 303'75){
-    ledBelt[13] = intense;
-  }
-  else if (angle > 303'75 && angle <= 326'25){
-    ledBelt[14] = intense;
-  }
-  else if (angle > 326'25 && angle <= 348'75){
-    ledBelt[15] = intense;
-  }
+	if ((angle > 0 && angle <= 11'25) || (angle > 348'75 && angle <= 360)) {
+		ledBelt[0] = intense;
+	}
+	else if (angle > 11'25 && angle <= 33'75) {
+		ledBelt[1] = intense;
+	}
+	else if (angle > 33'75 && angle <= 56'25) {
+		ledBelt[2] = intense;
+	}
+	else if (angle > 56'25 && angle <= 78'75) {
+		ledBelt[3] = intense;
+	}
+	else if (angle > 78'75 && angle <= 101'25) {
+		ledBelt[4] = intense;
+	}
+	else if (angle > 101'25 && angle <= 123'75) {
+		ledBelt[5] = intense;
+	}
+	else if (angle > 123'75 && angle <= 146'25) {
+		ledBelt[6] = intense;
+	}
+	else if (angle > 146'25 && angle <= 168'75) {
+		ledBelt[7] = intense;
+	}
+	else if (angle > 168'75 && angle <= 191'25) {
+		ledBelt[8] = intense;
+	}
+	else if (angle > 191'25 && angle <= 213'75) {
+		ledBelt[9] = intense;
+	}
+	else if (angle > 213'75 && angle <= 236'25) {
+		ledBelt[10] = intense;
+	}
+	else if (angle > 236'25 && angle <= 258'75) {
+		ledBelt[11] = intense;
+	}
+	else if (angle > 258'75 && angle <= 281'25) {
+		ledBelt[12] = intense;
+	}
+	else if (angle > 281'25 && angle <= 303'75) {
+		ledBelt[13] = intense;
+	}
+	else if (angle > 303'75 && angle <= 326'25) {
+		ledBelt[14] = intense;
+	}
+	else if (angle > 326'25 && angle <= 348'75) {
+		ledBelt[15] = intense;
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	//if we would like to calibrate our filter values, set to true.
-	bool calibrationMode = false;
-
-	//Matrix to store each frame of the webcam feed
 	Mat cameraFeed;
-	Mat threshold;
-	Mat HSV;
-
-	//create vector of points
-
 	vector<vector<Point>>rectcount;
-
-	if (calibrationMode) {
-		//create slider bars for HSV filtering
-		createTrackbars();
-	}
-	//video capture object to acquire webcam feed
 	VideoCapture capture;
-	//open capture object at location zero (default location for webcam)
 	capture.open(0);
-	//set height and width of capture frame
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
-	//start an infinite loop where webcam feed is copied to cameraFeed matrix
-	//all of our operations will be performed within this loop
 	waitKey(1000);
 	while (1) {
-		//store image to matrix
 		capture.read(cameraFeed);
-
 		src = cameraFeed;
+		if (!src.data) return -1;
 
-		if (!src.data)
-		{
-			return -1;
-		}
-
-		//convert frame from BGR to HSV colorspace
-		cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
-
-		if (calibrationMode == true) {
-
-			//need to find the appropriate color range values
-			// calibrationMode must be false
-
-			//if in calibration mode, we track objects based on the HSV slider values.
-			cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
-			inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
-			morphOps(threshold);
-			imshow(windowName2, threshold);
-
-			//the folowing for canny edge detec
-			/// Create a matrix of the same type and size as src (for dst)
-			dst.create(src.size(), src.type());
-			/// Convert the image to grayscale
-			cvtColor(src, src_gray, CV_BGR2GRAY);
-			/// Create a window
-			namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-			/// Create a Trackbar for user to enter threshold
-			createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold);
-			/// Show the image
-			trackFilteredObject(threshold, HSV, cameraFeed);
-		}
-		else {
-			//create some temp fruit objects so that
-			//we can use their member functions/information
-			Object blue("blue"), yellow("yellow"), red("red"), green("green");
-
-			//first find blue objects
-			cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
-			inRange(HSV, blue.getHSVmin(), blue.getHSVmax(), threshold);
-			morphOps(threshold);
-			trackFilteredObject(blue, threshold, HSV, cameraFeed);
-			//then yellows
-			cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
-			inRange(HSV, yellow.getHSVmin(), yellow.getHSVmax(), threshold);
-			morphOps(threshold);
-			trackFilteredObject(yellow, threshold, HSV, cameraFeed);
-			//then reds
-			cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
-			inRange(HSV, red.getHSVmin(), red.getHSVmax(), threshold);
-			morphOps(threshold);
-			trackFilteredObject(red, threshold, HSV, cameraFeed);
-			//then greens
-			//cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
-			//inRange(HSV, green.getHSVmin(), green.getHSVmax(), threshold);
-			//morphOps(threshold);
-			//trackFilteredObject(green, threshold, HSV, cameraFeed);
-
-		}
-		//show frames
-		//imshow(windowName2,threshold);
+		vector<Point> player = trackPlayer(cameraFeed);
+		cv::line(cameraFeed, player.at(0), player.at(1), Scalar(110, 220, 0), 2, 8);
 
 		imshow(windowName, cameraFeed);
-		//imshow(windowName1,HSV);
-
-		//delay 30ms so that screen can refresh.
-		//image will not appear without this waitKey() command
 		waitKey(30);
 	}
 	return 0;
